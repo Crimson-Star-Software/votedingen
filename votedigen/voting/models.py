952a607ad0f8cc76be7fd9b7915a3f3844a6ccd4
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-02-19 17:27:12 -0500

    Added createat fields and email & title for the candidate

--- votedigen/voting/models.py
+++ votedigen/voting/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 class Candidate(models.Model):
     name = models.CharField(max_length=255)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    title = models.CharField(max_length=255)
 
 class Election(models.Model):
     title = models.CharField(max_length=255)
     isOpen = models.BooleanField(default=False)
     candidates = models.ManyToManyField(Candidate)
+    created_at = models.DateTimeField(auto_now_add=True)
