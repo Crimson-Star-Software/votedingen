cb3cdf61bf0f9236bcc89ae49379612f826570ac
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-02-20 16:29:16 -0500

    Added __repr__ and __str__ methods

--- votedingen/voting/models.py
+++ votedingen/voting/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 class Candidate(models.Model):
+    def __repr__(self):
+        return f"<Candidate {self.name}>"
+
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     email = models.EmailField()
     title = models.CharField(max_length=255)
 
 class Election(models.Model):
+    def __repr__(self):
+        return f"<Election '{self.title}'>"
+
+    def __str__(self):
+        return self.title
     title = models.CharField(max_length=255)
     isOpen = models.BooleanField(default=False)
     candidates = models.ManyToManyField(Candidate)
