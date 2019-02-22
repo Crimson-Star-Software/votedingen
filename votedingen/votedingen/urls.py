f1de8981ee8389cfffb22a98f8863595a9d3923a
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-02-22 13:42:37 -0500

    Refactored for organization

--- votedingen/votedingen/urls.py
+++ votedingen/votedingen/urls.py
@@ -16,19 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
